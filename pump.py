3fb757434dbb0a7bbf1b8a6537f2d7d6a5ed99ca
Janet van der Graaf <janet@tsf-460-wpa-1-248.epfl.ch>
2021-08-30 17:19:30 +0200

    new pump code

--- pump.py
+++ pump.py
@@ -1,5 +1,5 @@
 """
-ALL.py
+pump.py
 ~~~~~~~~~~~~~~~~~
 This program shows how to connect to to the LSPone (laboratory syringe pump) using python and do the General cleaning procedure.
 It should be done at the beginning and end of the day.
@@ -18,10 +18,6 @@ import sys
 import serial
 import time
 
-
-
-stop_pandp = False
-
 #%% Functions
 def initialise_LSPone(lsp):
     #Initialise LSPone
@@ -31,13 +27,11 @@ def initialise_LSPone(lsp):
 
 def general_cleaning_procedure(lsp):
     # 2: PBS, 3: Air, 4: Ethanol
-    lsp.write(b"/1V200M200B2M1000A1500M1000B6M1000A0M1000B3M1000A1500M1000B6M1000A0M1000B4M1000A1500M1000B6M1000A0M1000B3M1000A3000M1000B6M1000A0M1000B3M1000A3000M1000B6M1000A0M1000R\r")
+        lsp.write(b"/1V200M200B2M1000A1500M1000B6M1000A0M1000B3M1000A1500M1000B6M1000A0M1000B4M1000A1500M1000B6M1000A0M1000B3M1000A3000M1000B6M1000A0M1000B3M1000A3000M1000B6M1000A0M1000R\r")
 
 
 def pick_BB(lsp):
     lsp.write(b"/1V5M200B6M1000A480M1000V300M200B3M1000A510M1000B6M1000V5M200R\r")
-    time.sleep(105)
-    beep.beep(8)
 
 def push_pull_sample(lsp, n_times):
     if n_times == 1:
@@ -50,108 +44,29 @@ def push_pull_sample(lsp, n_times):
         lsp.write(b"/1V5M1000A660M1000A510M1000A660M1000A510M1000A660M1000A510M1000A660M1000A510M1000R\r")
 
 
-#%% Open serial connection -> check COM port on your device
-lsp = serial.Serial('COM5', 9600, timeout=1000)
-print('LSPone connected on ',lsp.name)
-#%% Initialise LSPone normally already done
-initialise_LSPone(lsp)
-
-#%% 0. General Cleaning # this were i start
-# 2: PBS, 3: Air, 4: Ethanol
-lsp.write(b"/1V200M200B2M1000A1500M1000B6M1000A0M1000B3M1000A1500M1000B6M1000A0M1000B4M1000A1500M1000B6M1000A0M1000B3M1000A3000M1000B6M1000A0M1000B3M1000A3000M1000B6M1000A0M1000R\r")
-
-#%% 1. Sippin part 1 (26/08)
-# Sucking BB at 50 ul/min
-pick_BB(lsp)
-
-#%% 2. Sippin part 2
-# 2x Push and pull at 50 ul/min
-lsp.write(b"/1V5M1000A660M1000A510M1000A660M1000A510M1000R\r")
-time.sleep(138)
-beep.beep(8)
-
-#%% 3x Push and pull at 50 ul/min
-lsp.write(b"/1V5M1000A660M1000A510M1000A660M1000A510M1000A660M1000A510M1000R\r")
-time.sleep(207)
-beep.beep(8)
-
-#%% 4x Push and pull at 50 ul/min
-lsp.write(b"/1V5M1000A660M1000A510M1000A660M1000A510M1000A660M1000A510M1000A660M1000A510M1000R\r")
-time.sleep(276)
-beep.beep(8)
-
-#%% 3. Push & pull (10 times)
-lsp.write(b"/1V5M1000A660M1000A510M1000A660M1000A510M1000A660M1000A510M1000A660M1000A510M1000A660M1000A510M1000A660M1000A510M1000A660M1000A510M1000A660M1000A510M1000A660M1000A510M1000A660M1000A510M1000R\r")
-# Print time in minutes after instruction is sent
-for i in range(1, 11):
-        time.sleep(69)
-        print(i)
-beep.beep(8)
-#%% 3. Push & pull (20 times)
-lsp.write(b"/1V5M1000A660M1000A510M1000A660M1000A510M1000A660M1000A510M1000A660M1000A510M1000A660M1000A510M1000A660M1000A510M1000A660M1000A510M1000A660M1000A510M1000A660M1000A510M1000A660M1000A510M1000A660M1000A510M1000A660M1000A510M1000A660M1000A510M1000A660M1000A510M1000A660M1000A510M1000A660M1000A510M1000A660M1000A510M1000A660M1000A510M1000A660M1000R\r")
-# Print time in minutes after instruction is sent
-for i in range(1, 21):
-        time.sleep(60)
-        print(i)
-#%% Empty syringe in trash
-#to make sure te syrine is fully pused out
-lsp.write(b"/1V150B1M1000A0M1000R\r")
-
-#%% Push & pull (2 times)
-n = 2
-push_pull_sample_n_times(lsp, n)
-stop_pandp = False
-#Each push and pull takes exactly 70s
-
-#%% Push & pull (3 times)
-n = 3
-push_pull_sample_n_times(lsp, n)
-stop_pandp = False
-#Each push and pull takes exactly 70s
-
-#%% Push & pull (4 times)
-n = 4
-push_pull_sample_n_times(lsp, n)
-stop_pandp = False
-#Each push and pull takes exactly 70s
-
-#%% Push & pull (8 times)
-lsp.write(b"/1V5M1000A660M1000A510M1000A660M1000A510M1000A660M1000A510M1000A660M1000A510M1000A660M1000A510M1000A660M1000A510M1000A660M1000A510M1000R\r")
-# Print time in minutes after instruction is sent
-for i in range(1, 9):
-        time.sleep(60)
-        print(i)
-#%% Air Cleanin
-lsp.write(b"/1V300B3M1000A1000M1000B6M1000A0M1000R\r")
-
-#%% IN CASE BB IS NOT MOVING
-# Start picking BB again at 100 ul/min 
-#Empty syringe 
-lsp.write(b"/1V150B1M1000A0M1000R\r")
-# Pick at 100 ul/min
-lsp.write(b"/1V5M200B6M1000A240M1000R\r")
-
-#%% Sucking 80 ul BB at 100 ul/min 
-lsp.write(b"/1V10M200B6M1000A480M1000R\r")
-#%%  Put in position A480 to do sample 
-lsp.write(b"/1V100M200B3M1000A480M1000R\r")
-
-#%% 2. Sippin part 2
-# 1x Push and pull at 200 ul/min
-lsp.write(b"/1V20M1000A660M1000A510M1000R\r")
-print(0)
+def general_cleaning_procedure_fast(lsp):
+    start_s = b"/1V300M200" #start with speed 3000 ul/min
+    pull = b"M1000A1500M1000"
+    push = b"B6M1000A0M1000"
+    end_s = b"R\r"
+    lsp.write(start_s + b"B2" + pull + push + b"B3" + pull + push + b"B4" + pull + push + end_s)
 
 
-#%%
-#Finishing the script 
-sys.exit(0)
+    # Push pull sample for 69s each
+def push_pull_sample_n_times(lsp,n):
+    start_s = b"/1V5M1000B6M1000"
+    push_pull = b"A660M1000A510M1000"
+    end_s = b"R\r"
+    lsp.write(start_s+n*push_pull+end_s)
 
-#%% 
-def willy():
-    beep.beep(7)
 
-    threading.Thread(target=willy).start()
-    time.sleep(0.1)
-    
-        
-threading.Thread(target=willy).start()
+def air_cleaning(lsp):
+    lsp.write(b"/1V300B3M1000A1000M1000B6M1000A0M1000R\r")
+
+
+def pick_BB_again(lsp):
+    # IN CASE BB IS NOT MOVING
+    # Start picking BB again at 100 ul/min
+    #Empty syringe
+    lsp.write(b"/1V150B1M1000A0M1000RV10M200B6M1000A480M1000R\r")
+
